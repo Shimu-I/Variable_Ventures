f94c1f30ec0c7362216731a0ea19eb0e9b04a47b
Shimu <shimu278274@gmail.com>
2024-01-22 23:54:50 +0600

    basic ongoing

---
+++ DSA/Pr/first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+std:: cout << "your age: ";
+cin >> age;
+std:: cout << "\n\n it's " << age;
+
+
+}
---
+++ DSA/practicing/first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    // Prompt the user to enter a number
+    std::cout << "Enter a number: ";
+
+    // Use cin to get input from the user
+    std::cin >> number;
+
+    // Display the entered number
+    std::cout << "You entered: " << number << std::endl;
+
+    return 0;
+}
