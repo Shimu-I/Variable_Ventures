dfd37fe11d366c4a794ab30319c7a7e9428b66dc
Shimu <shimu278274@gmail.com>
2024-01-30 02:28:18 +0600

    v9 DONE

--- DSA/practicing/v12.cpp
+++ DSA/practicing/v12.cpp
@@ -1,4 +1,7 @@
+
+/*
 #include <iostream>
+
 using namespace std;
 
 
@@ -35,5 +38,42 @@ cout << "binary search for even number: " << even_number << endl;
 
 int odd_number = binarySearch(odd, 5, 97);
 cout << "binary search for odd number: " << odd_number << endl; 
+
+}
+
+
+*/
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[],int n,int key){
+
+    int start = 0;
+    int end = n-1;
+    int mid = start + ((end - start)/2);
+
+    while( start <= end){
+
+        if ( key == arr[mid]){
+            return mid;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }
+        else if (key < arr[mid]){
+           end = mid -1;
+        }
+         mid = start + ((end - start)/2);
+    }
+    return -1;
+}
+
+int main(){
+
+    cout << "hey" << endl;
+int array[5] = {3, 23, 44, 98, 98};
+int found = binarySearch(array, 5, 98);
+cout << "KEY: " << found ;
+
 
 }
