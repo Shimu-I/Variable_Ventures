464e29a722c2a8ffc198816a5266ca5bb6afe8b8
Shimu <shimu278274@gmail.com>
2024-02-26 23:53:49 +0600

    Studing DSA

---
+++ DSA/practicing/v44_doubly_linklist.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+	int data;
+	Node* prev;
+	Node* next;
+
+	Node(int data){
+		this -> data = data;
+		this -> prev = NULL;
+		this -> next = NULL;
+			
+	}
+		~Node(){
+		 int value = this -> data;
+		 if(this -> next != NULL){
+			delete next;
+			this -> next = NULL;
+		}
+
+	}
+
+};
+
+void printNode(Node* &head){
+	Node* temp = head;
+	while(temp != NULL){
+		cout << temp -> data << " ";
+		temp = temp -> next;	
+	}
+cout << endl;
+
+}
+
+/*
+int insertFirst(Node* &head, int n){
+	Node* temp = new Node(n);
+	temp -> next = head;
+	head -> prev = temp;
+	head = temp;
+
+}
+
+*/
+
+
+int insertFirst(Node* &head,Node* &tail, int n){
+	if(head == NULL){
+		Node* temp = new Node(n);
+		head = temp;
+		tail = temp;
+	}
+	else{
+		Node* temp = new Node(n);
+		temp -> next = head;
+		head -> prev = temp;
+		head = temp;
+
+	}
+}
+
+/*
+int insertEnd(Node* &tail, int n){
+	Node* temp = new Node(n);
+	tail -> next = temp;
+	temp-> prev = tail;
+	tail = temp;
+}
+
+*/
+int insertEnd(Node* &tail,Node* &head, int n){
+	if(tail == NULL){
+		Node* temp = new Node(n);
+		head = temp;
+		tail = temp;
+	}
+	else{
+	Node* temp = new Node(n);
+	tail -> next = temp;
+	temp-> prev = tail;
+	tail = temp;
+	}
+}
+
+
+
+int insertAtPosition(Node* &head, Node* &tail, int pos, int d){
+	if(pos == 1){
+		insertFirst(head, tail,d);
+		return 0;	
+	}
+	Node* temp = head;
+	int count =1;
+	while(count < pos-1){
+		temp = temp -> next;
+		count++;
+	}
+
+	if(temp->next == NULL){
+		insertEnd(tail,head, d);
+		return 0;
+	}
+
+		Node* nodePH = new Node(d);
+		nodePH -> next = temp-> next;
+		temp -> next -> prev = nodePH ;
+		temp -> next = nodePH;
+		nodePH -> prev = temp;
+}
+
+
+
+int deletePos(Node* &head, int pos){
+
+
+	if(pos == 1){
+		Node* temp = head;
+		temp -> next -> prev = NULL;
+		head = temp -> next;
+		temp -> next= NULL;
+		delete temp;
+
+	}
+
+	else{
+	Node* prev = NULL;
+	Node* curr = head;
+	int count = 1;
+
+	while(count < pos){
+		prev = curr;
+		curr = curr -> next;
+		count++;
+	}	
+
+	
+		curr -> prev = NULL;
+		prev -> next = curr -> next;
+		curr -> next = NULL;
+		delete curr;
+
+	
+
+
+}
+}
+
+int getLength(Node* &head){
+	int length = 0;
+	Node* temp = head;
+	while(temp != NULL){
+		length++;
+		temp = temp -> next;	
+	}
+return length;
+}
+
+int main(){
+	
+	Node* head = NULL;
+    Node* tail = NULL;
+
+	printNode(head);
+
+    cout << endl;
+    //cout << "Head: " << head -> data << endl;
+    //cout << "Tail: " << tail -> data << endl;
+
+
+
+	insertFirst(head,tail, 87);
+	printNode(head);
+
+    insertFirst(head,tail, 77);
+	printNode(head);
+
+    insertFirst(head,tail, 67);
+	printNode(head);
+
+    insertEnd(tail,head, 399);
+	printNode(head);
+
+	insertAtPosition(head, tail, 5, 456);
+	printNode(head);
+
+    cout << "Length of the linl list: " << getLength(head) << endl;
+
+	deletePos(head, 5);
+	printNode(head);
+
+	cout << "Length of the linl list: " << getLength(head) << endl;
+
+	cout << "Head: " << head -> data << endl;
+    cout << "Tail: " << tail -> data << endl;
+}
---
+++ DSA/practicing/v44_singly_linklist.cpp
@@ -0,0 +1,378 @@
+/*
+
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+
+int traverseLinklist(struct node *head){
+    struct node *curr = head;
+    int count = 0;
+
+    while( curr != NULL){
+        count++;
+        curr = curr -> next;
+    }
+    return count;
+}
+int main(){
+
+
+}
+
+
+*/
+
+/*
+
+#include <iostream>
+using namespace std;
+
+
+class Node{
+	public:
+	int data;
+	Node * next;
+	
+	Node(int data){
+	this -> data = data;
+	this -> next = NULL;
+	}
+
+	~Node(){
+		int value = this -> data;
+		
+	if(this -> next != NULL){
+		delete next;
+		this -> next = NULL;
+		
+	}
+	}
+};
+
+int insertFront(Node* &head , int n){
+	Node* node2 = new Node(n);
+	node2 -> next = head;
+	head = node2;
+
+}
+
+int insertEnd(Node* &tail, int n){
+	Node* temp = new Node(n);
+	tail -> next = temp;
+	tail = temp;
+	
+
+}
+
+
+int insertPosi(Node* &head,Node* &tail, int pos,int n ){
+	
+
+	if(pos == 1)
+	{
+		insertFront(head,n);
+		return 0;
+	}
+	Node* temp = head;
+	int count = 1;
+	
+	if(temp -> next == NULL){
+		insertEnd(tail, n);
+	}
+
+	while(count < pos-1){
+		temp = temp -> next;
+		count++;
+	}
+	Node* nodeP = new Node(n);
+	nodeP -> next  = temp -> next;
+	temp -> next = nodeP;
+	
+
+}
+
+
+int deleteNode(Node* &head, int pos){
+	if(pos == 1){
+		Node* temp = head;
+		head = head -> next;
+		temp -> next = NULL;
+		delete temp;
+
+	}
+	else{
+	int count = 1;
+	Node* pre = NULL;
+	Node* curr = head;
+	while(count < pos){
+		pre = curr;
+		curr = curr -> next;
+	count++;				
+	}
+	pre -> next = curr -> next;
+	curr -> next = NULL;
+	delete curr;
+	}
+
+
+
+
+
+}
+
+int printLinkList(Node* &head){
+	Node* temp = head;
+	
+
+	while(temp != NULL){
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	
+		
+	}
+
+
+
+}
+int main(){
+	Node* node1 = new Node(10);
+
+	cout << node1 -> data << endl;
+	//cout << node1 -> next << endl;
+
+	Node* head = node1;
+	insertFront(head, 13);
+	printLinkList(head);
+	
+	cout << endl;
+
+	Node* tail = node1;
+	insertEnd(tail, 30);
+	printLinkList(head);
+	
+	cout << endl;
+	
+	insertPosi(head,tail, 3, 700);
+	printLinkList(head);
+	
+	cout << endl;
+	cout << "Head: " << head -> data << endl;
+	cout << "Tail: " << tail -> data << endl;
+	cout << endl;
+
+	cout << "Before deleting" << endl;
+	printLinkList(head);
+	deleteNode(head, 4);
+
+	cout << endl;
+	cout << "After deleting" << endl;
+	printLinkList(head);
+	
+	cout << endl;
+	cout << "Head: " << head -> data << endl;
+	cout << "Tail: " << tail -> data << endl;
+
+	
+	
+}
+
+*/
+
+
+
+
+
+
+
+# include <iostream>
+using namespace std;
+
+//creating a node template for linklist
+class Node{
+	public:
+	int data;
+	Node* next;
+	
+//Node constructor
+	Node(int data){
+	this -> data = data;
+	this -> next = NULL; 	
+		}
+
+		~Node(){
+		int value = this -> data;
+		if(this -> next != NULL){
+			delete next;
+			this -> next = NULL;
+		}
+
+	}
+};
+
+
+int insertStart(Node* &head, int n){
+	Node* temp = new Node(n);
+	temp -> next = head;
+	head = temp;
+
+}
+
+int insertEnd(Node* &tail, int n){
+	Node* temp = new Node(n);
+	tail -> next = temp;
+	tail = temp;
+
+}
+
+
+int insertPos(Node* &head,Node* &tail,int pos, int n){
+
+	Node* temp = head;
+	if(pos == 1){
+		insertStart(head, n);
+		return 0;
+	}
+	int count = 1;
+	
+	while(count < pos-1){
+		temp = temp -> next;
+		count++;
+	}
+
+	if(temp -> next  == NULL){
+		insertEnd(tail, n);
+		return 0;
+	}
+
+	Node* nodePH = new Node(n);
+	nodePH -> next = temp -> next;
+	temp -> next = nodePH;
+
+}
+
+
+int deletePos(Node* &head, int pos){
+
+
+	if(pos == 1){
+		Node* temp = head;
+		head = head -> next;
+		temp -> next = NULL;
+		delete temp;
+
+
+	}
+
+	else{
+	Node* pre = NULL;
+	Node* curr = head;
+	int count = 1;
+
+	while(count < pos){
+		pre = curr;
+		curr = curr -> next;
+		count++;
+	}	
+
+	
+	pre -> next  =  curr -> next;
+	curr -> next = NULL;
+	delete curr;
+
+	
+
+
+}
+}
+
+
+int deleteV(Node* &head, int value){
+
+
+	if(head -> data == value){
+		Node* temp = head;
+		head = head -> next;
+		temp -> next = NULL;
+		delete temp;
+
+
+	}
+
+	else{
+
+	Node* pre = NULL;
+	Node* curr = head;
+
+		while (curr != NULL) {
+    		if (curr->data == value) {
+				delete curr;
+    		} 
+			pre = curr;	
+			pre->next = curr->next;
+			curr = curr->next;
+			
+		}
+		}
+}
+
+
+
+int printNode(Node* &head){
+	Node* temp = head;
+	
+	while(temp != NULL){
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+cout << endl;
+
+}
+int main(){
+	//creating node object by attocating new memory
+	Node* node1 = new Node(14);
+	Node* head = node1;
+
+
+	cout<<endl;
+	printNode(head);
+	cout << "Head: " << head -> data << endl;
+	cout << "Tail: " << head -> next << endl;
+
+	cout<<endl;
+	cout << "Inserting in the head: ";
+	insertStart(head, 77);
+	printNode(head);
+
+	cout<<endl;
+	Node* tail = node1;
+	cout << "Inserting in the End: ";
+	insertEnd(tail, 56);
+	printNode(head);
+
+
+	cout<<endl;
+	cout << "Inserting in any position: ";
+	insertPos(head,tail,2, 696);
+	printNode(head);
+
+
+	cout<<endl;
+	cout << "After deletation with node position: ";
+	deletePos(head, 2);
+    printNode(head);
+
+	cout<<endl;
+	cout << "After deletation with value: ";
+	deleteV(head, 14);
+    printNode(head);
+
+	cout << endl;
+	cout << "Head: " << head -> data << endl;
+	cout << "Tail: " << head -> next << endl;
+
+}
