c8fb6f4bd46b6d422fa00b88549bb5296d25badd
Shimu <jasmin851t626@gmail.com>
2024-03-30 02:46:53 +0600

    Ruby

---
+++ DSA/Code Implementation/4 Ruby_pl/MY_name.rb
@@ -0,0 +1,5 @@
+puts "H      H  EEEEE  L      L        OOO    SSSSS  H      H  IIIII  M     M  U     U"
+puts "H      H  E       L      L       O   O  S       H      H    I    MM   MM  U     U"
+puts "HHHHHHHH  EEEE    L      L       O   O  SSSSS   HHHHHHH    I    M M M M  U     U"
+puts "H      H  E       L      L       O   O      S   H      H    I    M  M  M  U     U"
+puts "H      H  EEEEE  LLLLL  LLLLL    OOO   SSSSS   H      H  IIIII  M     M   UUUUU "
