77c8a6188bdc06363d77c68c1bfaa0106347c41a
Shimu <jasmin851t626@gmail.com>
2024-04-14 19:51:35 +0600

    learning

--- Python_Work/first.py
+++ Python_Work/first.py
@@ -1,1 +1,14 @@
-print("My first python program")
+print('start')
+
+def print_this(lang):
+    if lang == 'a' :
+        print('later later')
+    elif lang == 'b' :   
+        print('ka ka ka ka ')
+    else :
+        print('ksdgfegj ')
+
+print_this('dfgd')
+print_this('a')
+print_this('b')
+
